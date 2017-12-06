61346739640d6cdb01e8bcb9c661d0c54a524919
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2017-12-06 12:21:45 +0100

    remove foregin key line

--- kanbanboard/app/Artifact.php
+++ kanbanboard/app/Artifact.php
@@ -8,6 +8,6 @@ class Artifact extends Model
 {
   protected $table = 'artifacts';
   protected $primaryKey = 'id';
-  protected $foreignKey= 'projectId';
+  //protected $foreignKey= 'projectId';
   protected $fillable = ['title', 'createdDate','status', 'description','assignedTo'];
 }
