9873028e18da423783c41a349054fdbbeb3e0d01
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2017-12-07 23:42:07 +0100

    Removed timestamps from artifact database

--- kanbanboard/app/Artifact.php
+++ kanbanboard/app/Artifact.php
@@ -8,6 +8,5 @@ class Artifact extends Model
 {
   protected $table = 'artifacts';
   protected $primaryKey = 'id';
-  //protected $foreignKey= 'projectId';
-  protected $fillable = ['title', 'createdDate','status', 'description','assignedTo'];
+  protected $fillable = ['title','createdDate','status','description','assignedTo'];
 }
