e7d7691617522947182332db8a873f466a1e6d15
jawidnasiri <33424483+jawidnasiri@users.noreply.github.com>
2017-12-08 10:01:03 +0100

    Create projects model and migration. (#107)
    * Projects
    
    Model and migration for projects
    
    * new chenge
    
    Project_id as primary

---
+++ kanbanboard/database/migrations/2017_12_07_141304_projects.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Projects extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('project_id');
+            $table->string('createdBy');
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
