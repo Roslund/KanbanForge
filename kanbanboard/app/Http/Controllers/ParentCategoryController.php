ecab77ee3577b4f1006db8f3247e09b45c65165a
Anton Roslund <enari@minit.nu>
2017-12-09 08:56:09 +0100

    Updated middleware parentCategoryController

--- kanbanboard/app/Http/Controllers/ParentCategoryController.php
+++ kanbanboard/app/Http/Controllers/ParentCategoryController.php
@@ -18,7 +18,7 @@ class ParentCategoryController extends Controller
 
     public function __construct()
     {
-      $this->middleware('auth:admin');
+      $this->middleware('userisadmin');
     }
     /**
      * Display a listing of the resource.
