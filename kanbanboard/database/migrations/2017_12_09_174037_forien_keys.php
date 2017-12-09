577746a567579e7c01f8eb161a761d4addea8b48
Icepool <rds15001@student.mdh.se>
2017-12-09 19:21:20 +0100

    made the foreign key migration also drop the foreign keys when down() is run

--- kanbanboard/database/migrations/2017_12_09_174037_forien_keys.php
+++ kanbanboard/database/migrations/2017_12_09_174037_forien_keys.php
@@ -31,6 +31,16 @@ class ForienKeys extends Migration
      */
     public function down()
     {
-        //
+      Schema::table('categories', function ($table) {
+          $table->dropForeign(['parentcategory']);
+      });
+
+      Schema::table('cards', function ($table) {
+          // Providing an array with all value didn't work, so I'm splitting it up.
+          // This syntax means we don't have to provide the table name and the columns.
+          $table->dropForeign(['artifact_id']);
+          $table->dropForeign(['category_id']);
+          $table->dropForeign(['swimlane_id']);
+      });
     }
 }
