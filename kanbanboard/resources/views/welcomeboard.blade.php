4315799ca737c26af3a3c6f10a9c40bd897dd24f
Icepool <rds15001@student.mdh.se>
2017-12-12 15:27:35 +0100

    added check for if any categories were returned. If none were then the board isn't rendered and a message is shown to the user.

--- kanbanboard/resources/views/welcomeboard.blade.php
+++ kanbanboard/resources/views/welcomeboard.blade.php
@@ -10,67 +10,69 @@
 
 
 <body class="signup">
+  @if(count($categories) > 0)
+    <table>
+      @if(count($parentCategories) > 0)
+        <tr>
+          <th></th>
+          <?php $emptyColumns = 0; ?>
+          @foreach($categories as $category)
+            @if(is_null($category['parentcategory']))
+              <?php $emptyColumns++; ?>
+            @endif
+          @endforeach
+          <th colspan="{{$emptyColumns++}}"></th>
 
-  <table>
-    @if(count($parentCategories) > 0)
+          <?php $colSpanForParentCategory = 0; ?>
+          @foreach($parentCategories as $parentCategory)
+            @foreach($categories as $category)
+              @if($category['parentcategory'] == $parentCategory['id'])
+                <?php $colSpanForParentCategory++; ?>
+              @endif
+            @endforeach
+            <th colspan="{{$colSpanForParentCategory}}">{{$parentCategory['name']}}</th>
+            <?php $colSpanForParentCategory = 0 ?>
+          @endforeach
+        </tr>
+      @endif
       <tr>
         <th></th>
-        <?php $emptyColumns = 0; ?>
         @foreach($categories as $category)
-          @if(is_null($category['parentcategory']))
-            <?php $emptyColumns++; ?>
-          @endif
-        @endforeach
-        <th colspan="{{$emptyColumns++}}"></th>
-
-        <?php $colSpanForParentCategory = 0; ?>
-        @foreach($parentCategories as $parentCategory)
-          @foreach($categories as $category)
-            @if($category['parentcategory'] == $parentCategory['id'])
-              <?php $colSpanForParentCategory++; ?>
-            @endif
-          @endforeach
-          <th colspan="{{$colSpanForParentCategory}}">{{$parentCategory['name']}}</th>
-          <?php $colSpanForParentCategory = 0 ?>
+          <th>{{ $category["name"] }}</th>
         @endforeach
       </tr>
-    @endif
-    <tr>
-      <th></th>
-      @foreach($categories as $category)
-        <th>{{ $category["name"] }}</th>
-      @endforeach
-    </tr>
 
-    @foreach($swimlanes as $swimlane)
-    <tr>
-      <td>{{ $swimlane["name"] }}</td>
-      @foreach($categories as $category)
-        <td id="c{{$category['id']}}s{{$swimlane['id']}}" category_id='{{$category["id"]}}' swimlane_id='{{$swimlane["id"]}}'>
-          @foreach($cards as $card)
-            @if($card["swimlane_id"] == $swimlane["id"] && $card["category_id"] == $category["id"])
-              <div class='card' id="card{{$card['id']}}">Card {{ $card["id"] }}</div>
-            @endif
-          @endforeach
-        </td>
+      @foreach($swimlanes as $swimlane)
+      <tr>
+        <td>{{ $swimlane["name"] }}</td>
+        @foreach($categories as $category)
+          <td id="c{{$category['id']}}s{{$swimlane['id']}}" category_id='{{$category["id"]}}' swimlane_id='{{$swimlane["id"]}}'>
+            @foreach($cards as $card)
+              @if($card["swimlane_id"] == $swimlane["id"] && $card["category_id"] == $category["id"])
+                <div class='card' id="card{{$card['id']}}">Card {{ $card["id"] }}</div>
+              @endif
+            @endforeach
+          </td>
+        @endforeach
+      </tr>
       @endforeach
-    </tr>
-    @endforeach
 
-    <tr>
-      <td>&nbsp;</td>
-      @foreach($categories as $category)
-        <td id="c{{$category['id']}}snull" category_id='{{$category["id"]}}' swimlane_id='null'>
-          @foreach($cards as $card)
-            @if(is_null($card["swimlane_id"]) && $card["category_id"] == $category["id"])
-              <div class='card' id="card{{$card['id']}}">Card {{ $card["id"] }}</div>
-            @endif
-          @endforeach
-        </td>
-      @endforeach
-    </tr>
-  </table>
-
+      <tr>
+        <td>&nbsp;</td>
+        @foreach($categories as $category)
+          <td id="c{{$category['id']}}snull" category_id='{{$category["id"]}}' swimlane_id='null'>
+            @foreach($cards as $card)
+              @if(is_null($card["swimlane_id"]) && $card["category_id"] == $category["id"])
+                <div class='card' id="card{{$card['id']}}">Card {{ $card["id"] }}</div>
+              @endif
+            @endforeach
+          </td>
+        @endforeach
+      </tr>
+    </table>
+  @else
+    <h3>The administrator hasn't defined a board, please wait for them to do</h3>
+  @endif
 </body>
 
 @endsection
