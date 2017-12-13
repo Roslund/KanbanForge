f686ce42d94c92574c92ea70af879b7864154615
Dzana Hanic <dzana.1h@gmail.com>
2017-12-13 10:48:13 +0100

    deteled routes

--- kanbanboard/routes/web.php
+++ kanbanboard/routes/web.php
@@ -25,20 +25,16 @@ Route::get('welcome', function () {
 Route::get('/logout', 'Auth\UsersLoginController@logout');
 
 
+
 Route::prefix('admin')->group(function(){
 
 	/*categories*/
 	Route::resource('/categories', 'CategoryController');
 
-	/*Route::get('/categories/{category}',
-		['as' => 'admin.categories', 'uses' => 'CategoryController@show']);
 
 	/*swimlanes*/
 	Route::resource('/swimlanes', 'SwimlaneController');
 
-	/*Route::get('/swimlanes/{swimlane}',
-		['as' => 'admin.swimlanes', 'uses' => 'SwimlaneController@show']);
-	*/
 
 	/*parentcategories*/
 	Route::resource('/parentcategories', 'ParentCategoryController');
