3db7006badd56e53c004dd458e973eeb518cc2ef
Anton Roslund <enari@minit.nu>
2017-12-13 14:43:35 +0100

    Project save redirect to wrong page

--- kanbanboard/app/Http/Controllers/ProjectController.php
+++ kanbanboard/app/Http/Controllers/ProjectController.php
@@ -42,7 +42,7 @@ class ProjectController extends Controller
           }
 
         }
-          return redirect('/welcome');
+          return redirect('/board');
       }
       return back();
     }
