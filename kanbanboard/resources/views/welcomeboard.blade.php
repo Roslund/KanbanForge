761b1ba08a6dd2a051ec15ff53e8af839087dd7b
André Caldegren <andweij@gmail.com>
2017-12-14 12:04:54 +0100

    nicer borders and colors

--- kanbanboard/resources/views/welcomeboard.blade.php
+++ kanbanboard/resources/views/welcomeboard.blade.php
@@ -2,86 +2,70 @@
 
 @section('head')
 
-    <title>Board</title>
+<title>Board</title>
 
 @endsection
 
 @section('content')
 
-<body class="signup">
-  <!--Style-->
-  <h1>Kanban board</h1>
-  <!--<div class="tbl-header">
-    <table cellpadding="0" cellspacing="0" border="0">
-      <thead>
-        <tr>
-          <th>Code</th>
-          <th>Company</th>
-          <th>Price</th>
-          <th>Change</th>
-          <th>Change %</th>
-        </tr>
-      </thead>
-    </table>
-  </div>-->
+<h1>Kanban Board</h1>
 
-  <div class="tbl-header">
-      @if(count($categories) > 0)
-        <table cellpadding="0" cellspacing="0" border="0">
-          <tbody>
-          @if(count($parentCategories) > 0)
-            <tr>
-              <th></th>
-              <?php $emptyColumns = 0; ?>
-              @foreach($categories as $category)
-                @if(is_null($category['parentcategory']))
-                  <?php $emptyColumns++; ?>
-                @endif
-              @endforeach
-              <th colspan="{{$emptyColumns++}}"></th>
-
-              <?php $colSpanForParentCategory = 0; ?>
-              @foreach($parentCategories as $parentCategory)
-                @foreach($categories as $category)
-                  @if($category['parentcategory'] == $parentCategory['id'])
-                    <?php $colSpanForParentCategory++; ?>
-                  @endif
-                @endforeach
-                <th colspan="{{$colSpanForParentCategory}}">{{$parentCategory['name']}}</th>
-                <?php $colSpanForParentCategory = 0 ?>
-              @endforeach
-            </tr>
-          @endif
-          <tr>
-            <th></th>
-            @foreach($categories as $category)
-              <th>{{ $category["name"] }}</th>
-            @endforeach
-            @endif
-          </tr>
-        </tbody>
-    </table>
-  </div>
-
-  <div class="tbl-content">
+<div class="tbl-board">
   @if(count($categories) > 0)
-    <table cellpadding="0" cellspacing="0" border="0">
+  <table cellpadding="0" cellspacing="0" border="0">
+    <thead>
+      @if(count($parentCategories) > 0)
+        <tr>
+        <th class="empty-th"></th>
 
+        <?php $emptyColumns = 0; ?>
+        @foreach($categories as $category)
+          @if(is_null($category['parentcategory']))
+            <?php $emptyColumns++; ?>
+          @endif
+        @endforeach
+
+        <th colspan="{{$emptyColumns++}}"></th>
+
+        <?php $colSpanForParentCategory = 0; ?>
+        @foreach($parentCategories as $parentCategory)
+          @foreach($categories as $category)
+            @if($category['parentcategory'] == $parentCategory['id'])
+              <?php $colSpanForParentCategory++; ?>
+            @endif
+          @endforeach
+
+          <th colspan="{{$colSpanForParentCategory}}">{{$parentCategory['name']}}</th>
+
+          <?php $colSpanForParentCategory = 0 ?>
+        @endforeach
+        </tr>
+        @endif
+
+        <tr>
+        <th class="empty-th"></th>
+        @foreach($categories as $category)
+          <th>{{ $category["name"] }}</th>
+        @endforeach
+      </tr>
+    </thead>
+
+    <tbody>
       @foreach($swimlanes as $swimlane)
       <tr>
-        <th>{{ $swimlane["name"] }}</th>
-        @foreach($categories as $category)
-          <td id="c{{$category['id']}}s{{$swimlane['id']}}" category_id='{{$category["id"]}}' swimlane_id='{{$swimlane["id"]}}'>
-            @foreach($cards as $card)
-              @if($card["swimlane_id"] == $swimlane["id"] && $card["category_id"] == $category["id"])
-                <div class='card' id="card{{$card['id']}}">Card {{ $card["id"] }}
-                  Assignee:
-                  Description:
-                </div>
-              @endif
-            @endforeach
-          </td>
-        @endforeach
+      <th>{{ $swimlane["name"] }}</th>
+      @foreach($categories as $category)
+        <td id="c{{$category['id']}}s{{$swimlane['id']}}" category_id='{{$category["id"]}}' swimlane_id='{{$swimlane["id"]}}'>
+          @foreach($cards as $card)
+            @if($card["swimlane_id"] == $swimlane["id"] && $card["category_id"] == $category["id"])
+            <div class='card' id="card{{$card['id']}}">Card {{ $card["id"] }}
+              Assignee:
+              Description:
+            </div>
+            @endif
+          @endforeach
+        </td>
+      @endforeach
       </tr>
       @endforeach
 
@@ -91,22 +75,22 @@
           <td id="c{{$category['id']}}snull" category_id='{{$category["id"]}}' swimlane_id='null'>
             @foreach($cards as $card)
               @if(is_null($card["swimlane_id"]) && $card["category_id"] == $category["id"])
-                <div class='card' id="card{{$card['id']}}">
-                  Card {{ $card["id"] }}
-                </div>
+              <div class='card' id="card{{$card['id']}}">
+              Card {{ $card["id"] }}
+              </div>
               @endif
             @endforeach
           </td>
         @endforeach
       </tr>
     </tbody>
-    </table>
+  </table>
+
   @else
-    <h3>The administrator hasn't defined a board, please wait for them to do</h3>
+  <h3>The administrator hasn't defined a board, please wait for them to do</h3>
   @endif
 
-  </div>
-</body>
+</div>
 
 @endsection
 
@@ -119,14 +103,14 @@
 
 @section('kanbanBoardIncludes')
 
-  <!-- Kanban Board imports -->
-  <script src="{{URL::asset('js/kanbanBoard/boardFunctionality.js')}}"></script>
+<!-- Kanban Board imports -->
+<script src="{{URL::asset('js/kanbanBoard/boardFunctionality.js')}}"></script>
 
-  <link rel="stylesheet" type="text/css" href="{{ url('/css/board-v2.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ url('/css/board-v2.css') }}" />
 
-  <!-- This is to inject the current server timestamp into the cardsTimestamp variable -->
-  <!-- Can't inject it directly into the javascript as it doesn't get compiled with blade -->
-  <!-- This needs to be done after the javascript above has loaded in, which is why it's under it -->
-  <script type="text/javascript">cardsTimestamp = "{{date('Y-m-d H:i:s')}}";</script>
+<!-- This is to inject the current server timestamp into the cardsTimestamp variable -->
+<!-- Can't inject it directly into the javascript as it doesn't get compiled with blade -->
+<!-- This needs to be done after the javascript above has loaded in, which is why it's under it -->
+<script type="text/javascript">cardsTimestamp = "{{date('Y-m-d H:i:s')}}";</script>
 
 @endsection
