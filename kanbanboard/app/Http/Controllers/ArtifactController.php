70367364a7b40a43eacd3ba608e69b54714b7800
jawidnasiri <jawid-jan@hotmail.com>
2017-12-17 13:35:42 +0100

    Generating cards
    When user selects artifacts then it will generate the cards in database with same artifact_id.
    if user dont select any artifacts then it will redirect to the same page.

--- kanbanboard/app/Http/Controllers/ArtifactController.php
+++ kanbanboard/app/Http/Controllers/ArtifactController.php
@@ -4,21 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Artifact;
+use App\Card;
 
 
 
 use Illuminate\Support\Facades\Input;
 
 class ArtifactController extends Controller {
-    
-    
+
+
 	public function __construct() {
-      
+
     $this->middleware('userisadmin');
   }
 
   public function index() {
-        //Gets the artifacts from the database! :) 
+        //Gets the artifacts from the database! :)
     $artifacts = Artifact::all()->sortByDesc("id");
 
     return view('admin.artifacts.index',compact(['artifacts']));
@@ -26,17 +27,27 @@ class ArtifactController extends Controller {
 
 
    public function select(Request $request) {
-    
-      $ids = Input::get('id');
 
-      return view('admin.artifacts.selected', compact('ids'));
+      $ids = Input::get('id');
+      if($ids)
+      {
+        foreach ($ids as $id) {
+          $card = new Card;
+          $card->artifact_id = $id;
+          $card->category_id = 1;
+          $card->swimlane_id = Null;
+          $card->save();
+          }
+          return view('admin.artifacts.selected', compact('ids'));
+      }
+      return redirect('/admin/filter');
   }
 
 
   public function refresh(){
-    
+
     Artifact::refresh_all_artifacts_from_teamforge();
-      
+
       return redirect('/admin/filter');
     }
 
