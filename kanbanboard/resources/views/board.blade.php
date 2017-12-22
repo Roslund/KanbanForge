a8617c02cfee0c2b7d8cfd98ef2e3ad1c74a291c
Icepool <rds15001@student.mdh.se>
2017-12-22 03:45:01 +0100

    Added the title code to cards rendered in the null swimlane

--- kanbanboard/resources/views/board.blade.php
+++ kanbanboard/resources/views/board.blade.php
@@ -127,7 +127,16 @@
               @if(is_null($card["swimlane_id"]) && $card["category_id"] == $category["id"])
               <div class='card' id="card{{$card['id']}}" card_id="{{$card['id']}}" draggable="true" ondragstart="drag(event)">
                 <a onclick="cardModal({{ $card['id'] }})" href="#">
-                Card {{ $card["id"] }}
+                <?php
+                  $wordCutoff = 5;
+                  if(str_word_count($card["title"]) > $wordCutoff)
+                  {
+                    echo implode(' ', array_slice(explode(' ', $card["title"]), 0, $wordCutoff)) . "&hellip;";
+                  }
+                  else {
+                    echo $card["title"];
+                  }
+                ?>
                 </a><br>
                 Assigned to: {{ $card["assignedTo"] }}<br>
                 Last updated:<br>
