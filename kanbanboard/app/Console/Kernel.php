a7085340edd759f7852f8de572ef3eefb70882cd
Anton Roslund <enari@minit.nu>
2017-12-23 12:07:49 +0100

    Automaticlt refresh artefacts from team forge

--- kanbanboard/app/Console/Kernel.php
+++ kanbanboard/app/Console/Kernel.php
@@ -32,6 +32,10 @@ class Kernel extends ConsoleKernel
             \Log::info("Scheduler is refreshing projects from teamforge");
             Project::refresh_all_projects_from_teamforge();
         })->everyMinute();
+        $schedule->call(function () {
+            \Log::info("Scheduler is refreshing artifacts from teamforge");
+            Artifact::refresh_all_artifacts_from_teamforge();
+        })->everyMinute();
     }
 
     /**
