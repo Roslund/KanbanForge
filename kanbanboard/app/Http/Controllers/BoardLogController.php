02962d782086a534d09cef90ff2c0bff9ce1e34a
Icepool <rds15001@student.mdh.se>
2017-12-26 22:11:47 +0100

    Increased log row limit to 50

--- kanbanboard/app/Http/Controllers/BoardLogController.php
+++ kanbanboard/app/Http/Controllers/BoardLogController.php
@@ -10,7 +10,7 @@ class BoardLogController extends Controller
 {
     public function index()
     {
-      $events = BoardLog::join('users', 'users.id', '=', 'board_log.userId') ->select('board_log.*', 'users.username')->orderBy('board_log.id', 'desc')->limit(12)->get()->reverse();
+      $events = BoardLog::join('users', 'users.id', '=', 'board_log.userId') ->select('board_log.*', 'users.username')->orderBy('board_log.id', 'desc')->limit(50)->get()->reverse();
 
       return view('stats.boardLog')->with(array('events' => $events));
     }
